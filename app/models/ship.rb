f94af6b0d6001d5939f5b435560899eb7adc3ae9
Gigi M <mmgamao1@gmail.com>
2019-08-19 14:09:22 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,10 +3,10 @@ class Ship
 
   @@ships = []
 
-  def initialize(args)
-    @name = args[:name]
-    @type = args[:type]
-    @booty = args[:booty]
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
     @@ships << self
   end
 
